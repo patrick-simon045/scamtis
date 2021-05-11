1a6e7e37c3c06d15e505dd6023c928330c81409c
lau-jr <laurentdeusdedith74@gmail.com>
2021-05-11 15:53:55 +0300

    adding user in admin site

--- reports/admin.py
+++ reports/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from django.contrib.auth.models import Group
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from reports import forms
+from reports.models import *
 
-# Register your models here.
+
+class UserAdmin(BaseUserAdmin):
+    add_form = forms.UserCreationForm
+
+    list_display = ('username', )
+    # list_filter = ('is_admin',)
+
+    fieldsets = (
+        (None, {'fields': ('username',  'password')}),
+        # ('Permissions', {'fields': ('is_admin',)})
+    )
+    search_fields = ('username',)
+    ordering = ('username', )
+
+    filter_horizontal = ()
+admin.site.register(User, UserAdmin)
+admin.site.unregister(Group)
