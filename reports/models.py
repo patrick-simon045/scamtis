56364caa02a18b2f3c367b7e1b9a7ba3c81d0e18
lau-jr <laurentdeusdith74@gmail.com>
2021-05-11 03:30:39 -0700

    initial database design

--- reports/models.py
+++ reports/models.py
@@ -1,3 +1,150 @@
 from django.db import models
+from django.urls import reverse
+from django.core.validators import MaxValueValidator, MinValueValidator
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class User(AbstractUser):
+    pass
+
+
+class Program(models.Model):
+    program_name = models.CharField(max_length=50, primary_key=True)
+    program_span = models.IntegerField(
+        validators=[MaxValueValidator(4), MinValueValidator(1)])
+
+    def __str__(self):
+        return self.program_name
+
+
+class Course(models.Model):
+    course_code = models.CharField(max_length=50, primary_key=True)
+    course_name = models.CharField(max_length=50)
+    programs = models.ManyToManyField("Program", through='Program_Course')
+
+    def __str__(self):
+        return self.course_code
+
+    class Meta:
+        verbose_name = ("course")
+        verbose_name_plural = ("courses")
+
+    def get_absolute_url(self):
+        return reverse("course_detail", kwargs={"pk": self.pk})
+
+
+class Program_Course(models.Model):
+    course = models.ForeignKey(
+        'Course', related_name='course', on_delete=models.CASCADE)
+    program = models.ForeignKey(
+        'Program', related_name='program', on_delete=models.CASCADE)
+    year_of_study = models.IntegerField()
+
+    def __str__(self):
+        pass
+
+    def __unicode__(self):
+        pass
+
+
+class Lecturer(models.Model):
+    user = models.OneToOneField(
+        "User", verbose_name=("user"), on_delete=models.CASCADE)
+    role = models.ForeignKey(
+        'Role', related_name='lecturer', on_delete=models.SET('empty'))
+
+    class Meta:
+        verbose_name = ("lecturer")
+        verbose_name_plural = ("lecturers")
+
+    def __str__(self):
+        return self.user.last_name
+
+    def get_absolute_url(self):
+        return reverse("lecturer_detail", kwargs={"pk": self.pk})
+
+
+class Lecture_Course(models.Model):
+    lecturer = models.ForeignKey(
+        'Lecturer', related_name='lecturer', on_delete=models.CASCADE)
+    course = models.ForeignKey(
+        'Course', related_name='course', on_delete=models.CASCADE)
+    academic_year = models.CharField(max_length=5)
+
+    def __str__(self):
+        pass
+
+    def __unicode__(self):
+        pass
+
+
+class Role(models.Model):
+    role = models.CharField(max_length=30, primary_key=True)
+
+    def __str__(self):
+        return self.role
+
+
+class Student(models.Model):
+
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    middle_name = models.CharField(max_length=50, blank=True, null=True)
+    regno = models.CharField(max_length=13, primary_key=True)
+    program = models.ForeignKey(
+        'Program', related_name='program', on_delete=models.SET_NULL)
+
+    class Meta:
+        verbose_name = ("student")
+        verbose_name_plural = ("students")
+
+    def __str__(self):
+        return self.regno
+
+    def get_absolute_url(self):
+        return reverse("student_detail", kwargs={"pk": self.pk})
+
+
+class Assessment_Criteria(models.Model):
+    criteria_name = models.CharField(max_length=20, primary_key=True)
+
+    def __str__(self):
+        return self.criteria_name
+
+
+class Assessment(models.Model):
+    criteria = models.ForeignKey(
+        'Assessment_Criteria', related_name='assessment', on_delete=models.CASCADE)
+    course = models.ForeignKey(
+        'Course', related_name='assessment', on_delete=models.CASCADE)
+    academic_year = models.CharField(max_length=5)
+    date_taken = models.DateTimeField(verbose_name='Date', auto_now_add=True)
+    total_mark = models.IntegerField(verbose_name='Total marks',
+                                     validators=[MaxValueValidator(100), MinValueValidator(0)])
+    number_of_questions = models.IntegerField(
+        validators=[MinValueValidator(1)])
+    contribution = models.IntegerField(verbose_name='Contribution in Percentage',
+                                       validators=[MaxValueValidator(40), MinValueValidator(0)])
+
+    def __str__(self):
+        return f'{self.criteria} {self.academic_year}'
+
+
+class Result(models.Model):
+    score = models.PositiveIntegerField(
+        validators=[MaxValueValidator(100), MinValueValidator(0)])
+    code = models.ForeignKey(
+        'Course', related_name='result', on_delete=models.CASCADE)
+    student = models.ForeignKey("Student", verbose_name=(
+        "student"), on_delete=models.CASCADE)
+
+    class Meta:
+        verbose_name = ("result")
+        verbose_name_plural = ("results")
+        unique_together = ['code', 'student']
+
+    def __str__(self):
+        return f'{self.code} results'
+
+    def get_absolute_url(self):
+        return reverse("result_detail", kwargs={"pk": self.pk})
