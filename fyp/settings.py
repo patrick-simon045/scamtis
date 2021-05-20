28892e77fa817d19ed1881464f4f280cc49a5d77
lau-jr <laurentdeusdedith74@gmail.com>
2021-05-20 10:25:33 +0300

    addind rf ap

--- fyp/settings.py
+++ fyp/settings.py
@@ -20,7 +20,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'reports.apps.ReportsConfig'
+    'reports.apps.ReportsConfig',
+    'rest_framework'
 ]
 
 MIDDLEWARE = [
