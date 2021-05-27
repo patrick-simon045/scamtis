d10981640bcc092ac905e7c654251f367eb01a11
lau-jr <laurentdeusdedith74@gmail.com>
2021-05-27 16:52:18 +0300

    adding statiifiles dir

--- fyp/settings.py
+++ fyp/settings.py
@@ -106,7 +106,7 @@ USE_TZ = True
 
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # my custom settings
