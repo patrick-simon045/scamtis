69a4d7fef7233b5047cb3cb17686d8c962bf1e6f
lau-jr <laurentdeusdedith74@gmail.com>
2021-06-18 14:05:05 +0300

    qn result component

---
+++ fyp_frontend/src/pages/home_page/tabs/ue_questions_results.jsx
@@ -0,0 +1,11 @@
+import TitleBar from "../../../components/titleBar";
+const UEQuestionsResults = () => {
+  return (
+    <TitleBar
+      title="Select University Exam Questions Result to change"
+      action="ADD UNIVERSITY EXAM QUESTIONS RESULTS"
+      to="/home/ue_qn_results/add"
+    />
+  );
+};
+export default UEQuestionsResults;
