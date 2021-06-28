df3f57e4a2a930a4d8de333b550bc8a5060eea11
lau-jr <laurentdeusdedith74@gmail.com>
2021-06-28 14:54:25 +0300

    removing permission on routes

--- newBackend/newBackend/settings.py
+++ newBackend/newBackend/settings.py
@@ -47,10 +47,10 @@ INSTALLED_APPS = [
 ]
 
 REST_FRAMEWORK = {
-    'DEFAULT_PERMISSION_CLASSES': (
-        'rest_framework.permissions.IsAuthenticated',
-        'rest_framework.permissions.IsAdminUser',
-    ),
+    # 'DEFAULT_PERMISSION_CLASSES': (
+    #     'rest_framework.permissions.IsAuthenticated',
+    #     'rest_framework.permissions.IsAdminUser',
+    # ),
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.TokenAuthentication',  # <-- And here
     ],
