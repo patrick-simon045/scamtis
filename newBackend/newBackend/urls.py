d269d765f96d749a9f5760b4dbf683ea16505b13
lau-jr <laurentdeusdedith74@gmail.com>
2021-06-28 14:53:47 +0300

    include backendapi urls

--- newBackend/newBackend/urls.py
+++ newBackend/newBackend/urls.py
@@ -1,13 +1,11 @@
 from django.contrib import admin
 from rest_framework.authtoken.views import obtain_auth_token
 from django.urls import path, include
-from backendapi import urls
-from backendapi import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', admin.site.urls),
     # path('', views.export_users_xls, name='export_excel'),
-    path('api/', include(urls)),
+    path('api/', include('backendapi.urls')),
     path('token_auth/', obtain_auth_token, name='token_auth'),
 ]
